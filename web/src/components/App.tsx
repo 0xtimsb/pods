0d9f27a70b5dec6545035e5f319d3d41ceb0ed72
Smit Barmase <smitbarmase@outlook.com>
2021-02-08 22:36:08 +0530

    Style update

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -7,9 +7,12 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="h-screen bg-white">
-        <div className="h-16 flex justify-center border-b border-gray-200">
+        <div className="sticky top-0 h-16 flex justify-center border-b border-gray-200">
           <div className="w-full max-w-6xl px-16 flex items-center">
-            <div>Button</div>
+            <div className="text-gray-900 text-sm cursor-pointer">Login</div>
+            <div className="bg-gray-900 text-white px-2.5 py-2 rounded border border-gray-900 text-sm cursor-pointer hover:bg-white hover:text-black">
+              Sign Up
+            </div>
           </div>
         </div>
       </div>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
