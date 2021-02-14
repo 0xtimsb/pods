3920909be78691897c34bd73cb4014164c190287
Smit Barmase <smitbarmase@outlook.com>
2021-02-15 02:54:31 +0530

    example env

--- server/src/types/env.d.ts
+++ server/src/types/env.d.ts
@@ -1,11 +1,10 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     DATABASE_URL: string;
-    PORT: string;
     REDIS_URL: string;
     SESSION_SECRET: string;
     CORS_ORIGIN: string;
-    CLIENT_DOMAIN: string;
+    PORT: string;
     NODEMAILER_EMAIL: string;
     NODEMAILER_PASSWORD: string;
     GITHUB_CLIENT_SECRET: string;
