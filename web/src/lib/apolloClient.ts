2cf740741a1a1dc457340f4483ef77a5d85eb8ca
Smit Barmase <smitbarmase@outlook.com>
2021-02-15 01:06:51 +0530

    server url env fix

--- web/src/lib/apolloClient.ts
+++ web/src/lib/apolloClient.ts
@@ -3,7 +3,7 @@ import { __prod__ } from "../constants/constant";
 
 const client = new ApolloClient({
   uri: __prod__
-    ? "https://pod-backend.herokuapp.com/graphql"
+    ? process.env.REACT_APP_SERVER
     : "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
   credentials: "include",
